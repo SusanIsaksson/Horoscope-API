452ce278b47173863bd8160023353af9b62608b3
susanisaksson <susan.isaksson@medieinstitutet.se>
2020-12-18 19:37:14 +0100

    #7 Delete är inte klar och funktion i js saknas

--- server/deletehoroscope.php
+++ server/deletehoroscope.php
@@ -1,4 +1,35 @@
 <?php
+try {
 
+    session_unset();
+
+    require("/addhoroscope");
+
+    if(isset($_SERVER['REQUEST_METHOD'])) {
+
+        if($_SERVER['REQUEST_METHOD'] === "DELETE") {
+
+            if(isset($_SESSION["birthDate"])) {
+
+                unset($_SESSION['']);
+
+                echo json_encode(true)
+
+            
+            } elseif {
+                
+                echo json_encode(false) 
+            }
+        }
+    }
+    exit;
+
+} catch(ErrorException $err) {
+    echo json_encode(
+        array(
+            "Message" => $err -> getMessage(),
+            "Status" => $err -> getCode())
+    );
+}
 
 ?>
