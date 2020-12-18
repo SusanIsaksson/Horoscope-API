395ec599407bb03bcae0fb4ed0914a07cf5a9edc
susanisaksson <susan.isaksson@medieinstitutet.se>
2020-12-18 19:37:47 +0100

    #6 update php är inte klar.

--- server/updatehoroscope.php
+++ server/updatehoroscope.php
@@ -1,4 +1,35 @@
 <?php
+try {
+    session_start();
 
+    require("./addhoroscope.php");
 
+    if(isset($_SERVER['REQUEST_METHOD'])) {
+
+        if($_SERVER['REQUEST_METHOD'] === "POST") {
+
+            
+            if(isset($_POST["birthDate"])) {
+
+                
+                $_SESSION["birthDate"] = serialize($_POST["birthDate"]);
+
+                
+                echo json_encode(true);
+
+            } else {
+
+                throw new ErrorException("name is not set in body", 500);
+            }
+        }
+    }
+
+} catch(ErrorException $err) {
+    echo json_encode(
+        array(
+            "Message" => $err -> getMessage(),
+            "Status" => $err -> getCode()
+        )
+    );
+    }
 ?>
