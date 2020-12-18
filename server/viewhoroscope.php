445c21948201b39c693bd09bab4c5f07e4e86774
susanisaksson <susan.isaksson@medieinstitutet.se>
2020-12-18 19:38:17 +0100

    #5 view php är inte klar.

--- server/viewhoroscope.php
+++ server/viewhoroscope.php
@@ -1,3 +1,33 @@
 <?php
 
+try {
+
+    session_start();
+
+    require("./addhoroscope.php");
+
+    if(isset($_SERVER['REQUEST_METHOD'])) {
+
+        if($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+            if(isset($_SESSION["birthDate"])) {
+
+                echo json_encode(unserialize($_SESSION["birthDate"]));
+
+            } else {
+
+                echo json_encode(false);
+            }
+            exit;
+        }
+    }
+    
+
+} catch(Exception $err) {
+    echo json_encode(
+        array(
+            "Message" => $err -> getMessage(),
+            "Status" => $err -> getCode())
+    );
+}
 ?>
