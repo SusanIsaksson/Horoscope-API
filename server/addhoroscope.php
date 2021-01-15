35fda63f2563db6b56ea429d33a149bc19235a53
susanisaksson <susan.isaksson@medieinstitutet.se>
2021-01-15 14:53:42 +0100

    #13 Korrigering av uträkningen av horoskop och nu stämmer datum i hoppet mellan två stjärntecken

--- server/addhoroscope.php
+++ server/addhoroscope.php
@@ -4,40 +4,40 @@ function zodiac($month, $day) {
 
     $zodiac = "";
 
-    if (($month == 12 && $day > 22) || ($month == 1 && $day < 20)) {
+    if (($month == 12 && $day >= 22) || ($month == 1 && $day <= 20)) {
         $zodiac = "Stenbock";  
     }
-    elseif (($month == 1 && $day > 21) || ($month == 2 && $day < 18)) {
+    elseif (($month == 1 && $day >= 21) || ($month == 2 && $day <= 18)) {
         $zodiac = "Vattuman";
     }
-    elseif (($month == 2 && $day > 19) || ($month == 3 && $day < 20)) {
+    elseif (($month == 2 && $day >= 19) || ($month == 3 && $day <= 20)) {
         $zodiac = "Fisk";
     }
-    elseif (($month == 3 && $day > 21) || ($month == 4 && $day < 20)) {
+    elseif (($month == 3 && $day >= 21) || ($month == 4 && $day <= 20)) {
         $zodiac = "Vädur";
     }
-    elseif (($month == 4 && $day > 21) || ($month == 5 && $day < 21)) {
+    elseif (($month == 4 && $day >= 21) || ($month == 5 && $day <= 21)) {
         $zodiac = "Oxe";
     }
-    elseif (($month == 5 && $day > 22) || ($month == 6 && $day < 21)) {
+    elseif (($month == 5 && $day >= 22) || ($month == 6 && $day <= 21)) {
         $zodiac = "Tvilling";
     }
-    elseif (($month == 6 && $day > 22) || ($month == 7 && $day < 22)) {
+    elseif (($month == 6 && $day >= 22) || ($month == 7 && $day <= 22)) {
         $zodiac = "Kräfta";
     }
-    elseif (($month == 7 && $day > 23) || ($month == 8 && $day < 23)) {
+    elseif (($month == 7 && $day >= 23) || ($month == 8 && $day <= 23)) {
         $zodiac = "Lejon";
     }
-    elseif (($month == 8 && $day > 24) || ($month == 9 && $day < 22)) {
+    elseif (($month == 8 && $day >= 24) || ($month == 9 && $day <= 22)) {
         $zodiac = "Jungfru";
     }
-    elseif (($month == 9 && $day > 23) || ($month == 10 && $day < 23)) {
+    elseif (($month == 9 && $day >= 23) || ($month == 10 && $day <= 23)) {
         $zodiac = "Våg";
     }
-    elseif (($month == 10 && $day > 24) || ($month == 11 && $day < 22)) {
+    elseif (($month == 10 && $day >= 24) || ($month == 11 && $day <= 22)) {
         $zodiac = "Skorpion";
     }
-    elseif (($month == 11 && $day > 23) || ($month == 12 && $day < 21)) {
+    elseif (($month == 11 && $day >= 23) || ($month == 12 && $day <= 21)) {
         $zodiac = "Skytt";
     } 
     return $zodiac;
@@ -61,9 +61,7 @@ try {
 
                 $yourZodiac = zodiac($_POST["month"], $_POST["day"]);
 
-                /* if(empty($yourZodiac)) {
-                    throw new Exception("Zodiac date is not correct");
-                } */
+                /* if(empty($yourZodiac)) */
                 if(!isset($yourZodiac)) {
                     echo json_encode("Du måste fylla i ett födelsedatum...");
                 }
